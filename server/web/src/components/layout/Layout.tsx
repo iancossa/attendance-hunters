ba102f175826232c9dfc7ffb97763656e873f737
Eldo Macuácua <eldomacuacua@yahoo.com>
2025-09-03 18:12:08 +0530

    layout

--- server/web/src/components/layout/Layout.tsx
+++ server/web/src/components/layout/Layout.tsx
@@ -10,10 +10,14 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <div className="flex">
+      <div className="flex h-[calc(100vh-4rem)]">
         <Sidebar />
-        <main className="flex-1 p-4 lg:p-6">
-          {children}
+        <main className="flex-1 overflow-y-auto bg-gradient-to-br from-background via-background to-muted/20">
+          <div className="p-6 lg:p-8">
+            <div className="space-y-6">
+              {children}
+            </div>
+          </div>
         </main>
       </div>
     </div>
